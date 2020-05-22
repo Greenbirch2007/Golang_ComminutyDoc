d1214f012a852a85ae1f8a51fd48db2fad5ca637
Greenbirch2007 <291109028@qq.com>
2020-05-22 23:21:56 +0800

    练练go

---
+++ gg/g1.go
@@ -0,0 +1,26 @@
+
+
+if i < f(){
+	g()
+}
+
+
+if x > 0 {
+	return y
+}
+
+
+if err := file.Chmod(0664); err != nil{
+	log.Print(err)
+	return err
+}
+
+
+f ,err := os.Open(name)
+if err !=nil{
+	return err
+}
+
+codeUsing(f)
+
+f,
