2386d9619b44730d07a6fefa6f9f9d449a69dbfd
Greenbirch2007 <291109028@qq.com>
2020-05-31 22:01:39 +0800

    golang exercise

--- gg/g.go
+++ gg/g.go
@@ -1,19 +1,30 @@
 package main
 
-import (
-	"fmt"
-	"math"
+import "fmt"
+
+type ChipType int
+
+const (
+	None ChipType = iota
+	CPU // Z中央处理器
+	GPU //图形处理器
 )
 
-func main()  {
-	fmt.Println("int8 range:",math.MinInt8,math.MaxInt8)
-	fmt.Println("int16 range:",math.MinInt16,math.MaxInt16)
-	fmt.Println("int32 range:",math.MinInt32,math.MaxInt32)
-	fmt.Println("int64 range:",math.MinInt64,math.MaxInt64)
-	var a int32 = 1047483647
-	fmt.Printf("int32:0x%x %d\n",a,a)
-	b := int16(a)
-	fmt.Printf("int16: 0x%x %d\n",b,b)
-	var c float32 = math.Pi
-	fmt.Println(int(c))
+
+func (c ChipType) String() string{
+	switch c {
+	case None:
+		return "None"
+	case CPU:
+		return "CPU"
+	case GPU:
+		return "GPU"
+
+	}
+
+	return "N/A"
+}
+
+func main(){
+	fmt.Printf("%s %d", CPU, CPU)
 }
