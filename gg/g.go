f6afcbc2e3f8a74be48112935bad81d01c8b9278
Greenbirch2007 <291109028@qq.com>
2020-06-12 23:14:16 +0800

    6.12

--- gg/g.go
+++ gg/g.go
@@ -2,20 +2,25 @@ package main
 
 import "fmt"
 
+const (
+	SecondPerMinute = 60
+	SecondPerHour = SecondPerMinute * 60
+	SecondPerDay = SecondPerHour*24
+
+)
+
+func resolveTime(seconds int) (day int,hour int, minute int){
+	day = seconds /SecondPerDay
+	hour = seconds / SecondPerHour
+	minute = seconds / SecondPerMinute
+
+	return
+}
+
 func main(){
-	c := make(chan int)
-	go func(){
-		c <-1
-		c <-2
-		close(c)
-
-		c <-6
-
-
-	}()
-	for v:= range c{
-		fmt.Println(
-			v)
-	}
-
+	fmt.Println(resolveTime(1000))
+	_,hour,minute := resolveTime(12000)
+	fmt.Println(hour,minute)
+	day,_,_ :=resolveTime(909000)
+	fmt.Println(day)
 }
