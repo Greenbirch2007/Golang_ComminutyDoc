a175d75e472f19146035e007e473e6f8b3e8a7ca
Greenbirch2007 <291109028@qq.com>
2020-06-15 22:18:24 +0800

    6.15

--- gg/g.go
+++ gg/g.go
@@ -1,54 +1,33 @@
 package main
 
+
 import (
+	"flag"
 	"fmt"
-	"strings"
 )
 
 
-//字符串处理函数，传入字符串切片和处理链
-func StringProcess(list []string,chain []func(string) string){
-	//遍历每一个字符串
-	for index,str := range list{
-		//第一个需要处理的字符串
-		result := str
-		//遍历每一个处理链
-		for _,proc := range chain{
-			//输入一个字符串进行处理，返回数据作为处理
-			//出力链的输入
-			result = proc(result)
-		}
+var skillParam = flag.String("skill","","skill to use")
 
-		//将结果放回切片
-		list[index] =result
-	}
-}
-
-//自定义的移除前缀的吃函数
-func removePrefix(str string)string{
-	return strings.TrimPrefix(str,"go")
-}
 
 func main(){
-	//待处理的字符串列表
-	list := []string{
-		"go scann",
-		"go scann",
-		"go scann",
-		"go scann",
-	}
-	//处理函数链
-	chain := []func(string) string{
-		removePrefix,
-		strings.TrimSpace,
-		strings.ToUpper,
+	flag.Parse()
+
+	var skill = map[string]func(){
+		"fire": func() {
+			fmt.Println("chicken fire")
+		},
+		"run": func() {
+			fmt.Println("soldier run")
+		},
+		"fly": func() {
+			fmt.Println("angel fly")
+		},
 	}
 
-	// 处理字符串
-	StringProcess(list, chain)
-
-	// 输出处理好的字符串
-	for _, str := range list {
-		fmt.Println(str)
+	if f, ok := skill[*skillParam]; ok {
+		f()
+	} else {
+		fmt.Println("skill not found")
 	}
 }
