873a7d2171720e6f461a0070015bfb87660a40e7
Greenbirch2007 <291109028@qq.com>
2020-07-24 22:48:07 +0800

    7.24 chg

--- gg/g.go
+++ gg/g.go
@@ -1,33 +1,33 @@
 package main
 
-
 import (
-	"flag"
 	"fmt"
 )
 
-
-var skillParam = flag.String("skill","","skill to use")
-
-
-func main(){
-	flag.Parse()
-
-	var skill = map[string]func(){
-		"fire": func() {
-			fmt.Println("chicken fire")
-		},
-		"run": func() {
-			fmt.Println("soldier run")
-		},
-		"fly": func() {
-			fmt.Println("angel fly")
-		},
-	}
-
-	if f, ok := skill[*skillParam]; ok {
-		f()
-	} else {
-		fmt.Println("skill not found")
+func Sort(data Interface){
+	for i:=1;i< data.Len();i++{
+		for j:=i;j>0 && data.Less(j,i-1);j--{
+			data.Swap(j,j-1)
+		}
 	}
 }
+
+type Interface interface {
+	Len() int
+	Less(i,j int) bool
+	Swap(i,j int)
+}
+
+type testType struct {
+	a int
+	b string
+}
+
+func (t *testType)String(){
+	return fmt.Sprint(t.a) +" "+t.b
+}
+func main()  {
+	t:= &testType{88,"sunset ss"}
+	fmt.Println(t)
+}
+
