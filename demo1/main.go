93e99d5afcb219606fac1d62c5c58684ba349a66
Greenbirch2007 <291109028@qq.com>
2020-08-10 21:58:21 +0800

    8.10 chg

--- demo1/main.go
+++ demo1/main.go
@@ -2,24 +2,18 @@ package main
 
 import (
 	"fmt"
-	"sort"
+	"strings"
 )
 
 func main() {
-	//长度是线段，容量是射线
-	//var sliceA = make([]int,4,8)
-	//sliceA[0]=10
-	//sliceA[1]=10
-	//sliceA[2]=10
-	//fmt.Println(sliceA)
-	//s1 := "big"
-	//bytestr := []byte(s1)
-	//bytestr[0] ='p'
-	//fmt.Println(string(bytestr))
-	// string----byte----string
-	intL := []int{2,6,9,1,6,66}
+	var str = "how do you do"
+	var strSlice= strings.Split(str," ")
+	var strMap = make(map[string]int)
+	for _, v := range strSlice {
+		strMap[v]++
+	}
+	fmt.Println(strMap)
 
-	sort.Sort(sort.Reverse(sort.IntSlice(intL)))
-	fmt.Println(intL)
-}
+ }
+
 
