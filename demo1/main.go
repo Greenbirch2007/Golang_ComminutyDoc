df17beaf351c592576757ee4e33073a5d15e7abc
Greenbirch2007 <291109028@qq.com>
2020-08-11 19:55:05 +0800

    8.11 chg

--- demo1/main.go
+++ demo1/main.go
@@ -1,18 +1,53 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
+import "fmt"
+
+type myInt int
+
+func add (x,y int)int{
+	return x+y
+}
+func sub (x,y int)int{
+	return x-y
+}
+
+type calcType func(int,int)int
+
+func do(o string) calcType{
+	switch o {
+	case "+":
+		return add
+	case "-":
+		return sub
+	case "*":
+		return func(x,y int)int{
+			return x*y
+		}
+	default:
+		return nil
+	}
+}
+
 
 func main() {
-	var str = "how do you do"
-	var strSlice= strings.Split(str," ")
-	var strMap = make(map[string]int)
-	for _, v := range strSlice {
-		strMap[v]++
-	}
-	fmt.Println(strMap)
+	//匿名函数 匿名自执行函数
+	func(){
+		fmt.Println("这是匿名函数")
+	}()
+
+	f := func(x,y int) int{
+		a := x*y
+		return a
+
+	}(6,9)
+	fmt.Println(f)
+
+	//匿名函数自执行函数接收参数
+	func(x,y int){
+		fmt.Println(x*y)
+
+	}(9,9)
+
 
  }
 
